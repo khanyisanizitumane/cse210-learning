15fdb592f9eb203f1554cab83c8ae6d7eb4475a2
khanyisanizitumane <zitumanekhanyisani@gmail.com>
2024-04-28 18:18:20 +0200

    Prep 1 James Bond

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+         // Prompting the user for their first name
+        Console.Write("Enter your first name: ");
+        string firstName = Console.ReadLine();
+
+        // Prompting the user for their last name
+        Console.Write("Enter your last name: ");
+        string lastName = Console.ReadLine();
+
+        // Displaying the name in the specified format
+        Console.WriteLine("Your name is " + lastName + ", " + firstName + ", " + lastName);
     }
 }
