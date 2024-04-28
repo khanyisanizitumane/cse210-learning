c765a8babf107de499002d641ba447ba2df283e5
khanyisanizitumane <zitumanekhanyisani@gmail.com>
2024-04-28 18:24:41 +0200

    Grade Conditionals

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Prompt the user for their grade percentage
+        Console.Write("Enter your grade percentage: ");
+        double gradePercentage = Convert.ToDouble(Console.ReadLine());
+
+        // Initialize the letter grade variable
+        char letter;
+
+        // Determine the letter grade based on the grade percentage
+        if (gradePercentage >= 90)
+        {
+            letter = 'A';
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = 'B';
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = 'C';
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = 'D';
+        }
+        else
+        {
+            letter = 'F';
+        }
+
+        // Print the letter grade
+        Console.WriteLine("Your letter grade is: " + letter);
+
+        // Check if the user passed the course and display a message accordingly
+        if (letter != 'F')
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Don't worry, keep working hard and you'll improve next time!");
+        }
     }
 }
