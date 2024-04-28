ca6320c74c30b903bcaf5fcc8d10ab65a9fb43f5
khanyisanizitumane <zitumanekhanyisani@gmail.com>
2024-04-28 19:21:54 +0200

    Guess game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Generate a random magic number between 1 and 10
+        Random random = new Random();
+        int magicNumber = random.Next(1, 11);
+
+        Console.WriteLine("Welcome to Guess My Number Game!");
+        Console.WriteLine("I have chosen a magic number between 1 and 10.");
+        
+        // Initialize guess variable
+        int guess;
+
+        // Loop until the user guesses the correct number
+        do
+        {
+            Console.Write("What is your guess? ");
+            guess = Convert.ToInt32(Console.ReadLine());
+
+            // Check if the guess is higher, lower, or correct
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while (guess != magicNumber);
     }
 }
